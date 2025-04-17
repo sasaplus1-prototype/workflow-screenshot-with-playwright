fcaea6f17f05702314d21caf6fc1c1cf2deec760
sasaplus1 <sasaplus1@gmail.com>
2025-04-17 15:36:29 +0900

    feat: add playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  forbidOnly: !!process.env.CI,
+  fullyParallel: true,
+  globalSetup: require.resolve("./global-setup"),
+  globalTeardown: require.resolve("./global-teardown"),
+  reporter: "html",
+  retries: process.env.CI ? 2 : 0,
+  testDir: ".",
+  workers: process.env.CI ? 1 : undefined,
+  use: {
+    storageState: "auth.json",
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
