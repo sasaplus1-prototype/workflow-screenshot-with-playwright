48ae7281c4eab9fd7914f08bd465460f99d2ad24
sasaplus1 <sasaplus1@gmail.com>
2025-04-17 15:36:54 +0900

    chore: add global setup and global teardown

---
+++ global-setup.ts
@@ -0,0 +1,7 @@
+// import {  } from '@playwright/test';
+
+async function globalSetup(config: FullConfig) {
+  // noop
+}
+
+export default globalSetup;
---
+++ global-teardown.ts
@@ -0,0 +1,7 @@
+// import {  } from '@playwright/test';
+
+async function globalTeardown(config: FullConfig) {
+  // noop
+}
+
+export default globalTeardown;
